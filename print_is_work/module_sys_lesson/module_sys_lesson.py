c4a4cbd8b0336eb96f21fdb29d9c0336b67156b2
Nika <LA.N21@yandex.ru>
2026-07-21 15:03:58 +0300

    Remove unnecessary folders

--- print_is_work/module_sys_lesson/module_sys_lesson.py
+++
@@ -1,5 +0,0 @@
-import sys
-
-print("Версия Python:", sys.version)
-print("Операционная система:", sys.platform)
-print("Путь к файлам:", sys.path)
--- print_is_work/module_sys_lesson/stdin_menu.py
+++
@@ -1,6 +0,0 @@
-import sys
-
-print("Меню ресторана:")
-for line in sys.stdin:
-    dish = line.strip()
-    print("- ", dish)
